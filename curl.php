eca181de75ceab5aacc0e848c6c817556c7eb013
Leona <srt459vn31@gmail.com>
2016-08-19 08:12:45 +0000

    學習

--- curl.php
+++ curl.php
@@ -2,7 +2,6 @@
     $ch = curl_init();
     $url = "http://tt181.me/test0975313025.php";
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPGET, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['token:rd1']);
     curl_setopt($ch, CURLOPT_POST, true); // 啟用POST
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array( "id"=>"10", "name"=>"Leona")));
